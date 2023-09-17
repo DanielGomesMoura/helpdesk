b00328563a67259fe1fb13b4eac4c33cd8603265
DanielMoura <daniel.tecnicop@gmail.com>
2023-09-17 16:26:15 -0400

    Endpoint findById para Tecnico

--- src/main/java/com/Daniel/helpdesk/controller/TecnicoController.java
+++ src/main/java/com/Daniel/helpdesk/controller/TecnicoController.java
@@ -1,2 +1,24 @@
-package com.Daniel.helpdesk.controller;public class TecnicoController {
+package com.Daniel.helpdesk.controller;
+
+import com.Daniel.helpdesk.domain.Tecnico;
+import com.Daniel.helpdesk.service.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/tecnico")
+public class TecnicoController {
+
+    @Autowired
+    private TecnicoService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+        Tecnico obj = this.service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 }
--- src/main/java/com/Daniel/helpdesk/service/TecnicoService.java
+++ src/main/java/com/Daniel/helpdesk/service/TecnicoService.java
@@ -1,2 +1,20 @@
-package com.Daniel.helpdesk.service;public class TecnicoService {
+package com.Daniel.helpdesk.service;
+
+import com.Daniel.helpdesk.domain.Tecnico;
+import com.Daniel.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository repository;
+
+    public Tecnico findById(Integer id){
+        Optional<Tecnico> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
 }
