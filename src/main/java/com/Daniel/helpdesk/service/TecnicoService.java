505b313c52b01eb5224721d586ce96f10c041d3b
DanielMoura <daniel.tecnicop@gmail.com>
2023-09-17 17:37:29 -0400

    endpoint findAll para Tecnico

--- src/main/java/com/Daniel/helpdesk/service/TecnicoService.java
+++ src/main/java/com/Daniel/helpdesk/service/TecnicoService.java
@@ -6,6 +6,7 @@ import com.Daniel.helpdesk.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class TecnicoService {
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: "+ id));
+    }
+
+    public List<Tecnico> findAll(){
+        return repository.findAll();
     }
 }
