2466c031b611c707bc6e8094cb979edd68dbdee7
DanielMoura <daniel.tecnicop@gmail.com>
2024-06-09 22:40:11 -0400

    Implementado encode Senha Login

--- src/main/java/com/Daniel/helpdesk/config/SimpleCorsFilter.java
+++ src/main/java/com/Daniel/helpdesk/config/SimpleCorsFilter.java
@@ -17,8 +17,8 @@ public class SimpleCorsFilter implements Filter{
 	    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 	        HttpServletResponse response = (HttpServletResponse) res;
 	        HttpServletRequest request = (HttpServletRequest) req;
-	        response.setHeader("Access-Control-Allow-Origin", "https://help-desk-front-delta.vercel.app");
-	        //response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+	        //response.setHeader("Access-Control-Allow-Origin", "https://help-desk-front-delta.vercel.app");
+	        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
 	        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
 	        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
 	        response.setHeader("Access-Control-Max-Age", "3600");
--- src/main/java/com/Daniel/helpdesk/service/TecnicoService.java
+++ src/main/java/com/Daniel/helpdesk/service/TecnicoService.java
@@ -59,8 +59,10 @@ public class TecnicoService{
 	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
 		objDTO.setId(id);
 		Tecnico oldObj = findById(id);
+		if(!objDTO .getSenha().equals(oldObj.getSenha())){
+		  objDTO.setSenha(encoder.encode(objDTO.getSenha())); 
+		}
 		validaCpfEmail(objDTO);
-		objDTO.setSenha(encoder.encode(objDTO.getSenha())); 
 		oldObj = new Tecnico(objDTO);
 		return repository.save(oldObj);		
 	}
